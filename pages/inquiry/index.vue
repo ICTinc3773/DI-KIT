2091303b1830006fa3a667d6f1d7ffa128d60c58
長尾 一矢 <k.nagao@hq.ictinc.co.jp>
2025-10-08 13:23:35 +0900

    マークアップ修正

--- pages/inquiry/index.vue
+++ pages/inquiry/index.vue
@@ -1,130 +1,126 @@
 <template>
-	<div class="site">
-		<main class="site--body">
-			<section class="inquiry">
-				<div class="container">
-					<header class="inquiry--header">
-						<header>
-							<PageTitle title-text="フォーム画面サンプル" />
-							<p>このページはDI-KIT内でBootstrap5を使用し、CSS命名規則を混在させても安全であることを示すサンプルです。</p>
-							<p>
-								不要な場合に削除しやすいようBootstrapのコアファイルはCDNで読み込んでいますが、実際に使用する場合は<a href="https://www.npmjs.com/package/bootstrap/v/5.0.0-alpha1">npm install</a
-								>を推奨しています。
-							</p>
-						</header>
-					</header>
-					<div class="inquiry--body">
-						<!-- フォーム全体 -->
-						<form @submit.prevent="onSubmit">
-							<!-- 名前フィールド -->
-							<div class="form-group">
-								<label for="name">名前<span class="required">*</span></label>
-								<!-- 入力フィールド -->
-								<Field name="name" type="text" id="name" class="form-control" />
-								<!-- バリデーションエラー表示 -->
-								<ErrorMessage name="name" class="error-message" />
-							</div>
-
-							<!-- 年齢フィールド -->
-							<div class="form-group">
-								<label for="age">年齢</label>
-								<!-- 数値入力用フィールド -->
-								<Field name="age" type="text" id="age" class="form-control" />
-								<!-- バリデーションエラー表示 -->
-								<ErrorMessage name="age" class="error-message" />
-							</div>
-
-							<!-- メールアドレスフィールド -->
-							<div class="form-group">
-								<label for="email">メールアドレス<span class="required">*</span></label>
-								<!-- メール入力フィールド -->
-								<Field name="email" type="email" id="email" class="form-control" />
-								<!-- バリデーションエラー表示 -->
-								<ErrorMessage name="email" class="error-message" />
-							</div>
-
-							<!-- 性別フィールド -->
-							<div class="form-group">
-								<label>性別<span class="required">*</span></label>
-								<div class="inline-group">
-									<!-- 男性選択 -->
-									<Field name="gender" type="radio" value="男性" id="male" />
-									<label for="male">男性</label>
-									<!-- 女性選択 -->
-									<Field name="gender" type="radio" value="女性" id="female" />
-									<label for="female">女性</label>
-								</div>
-								<!-- バリデーションエラー表示 -->
-								<ErrorMessage name="gender" class="error-message" />
-							</div>
-
-							<!-- 都道府県選択 -->
-							<div class="form-group">
-								<label for="prefecture">都道府県名<span class="required">*</span></label>
-								<!-- ドロップダウン選択 -->
-								<Field name="prefecture" as="select" id="prefecture" class="form-control">
-									<option value="" disabled>選択してください</option>
-									<!-- 各都道府県の選択肢 -->
-									<option value="北海道">北海道</option>
-									<!-- 以下省略 -->
-								</Field>
-								<!-- バリデーションエラー表示 -->
-								<ErrorMessage name="prefecture" class="error-message" />
-							</div>
-
-							<!-- メッセージフィールド -->
-							<div class="form-group">
-								<label for="message">メッセージ</label>
-								<!-- テキストエリア -->
-								<Field name="message" as="textarea" id="message" class="form-control" />
-								<!-- バリデーションエラー表示 -->
-								<ErrorMessage name="message" class="error-message" />
-							</div>
-
-							<!-- 同意確認フィールド -->
-							<fieldset class="form-group">
-								<legend>同意確認<span class="required">*</span></legend>
-								<div class="inline-group">
-									<!-- プライバシーポリシーの同意チェック -->
-									<Field name="privacyPolicy" type="checkbox" id="privacyPolicy" :value="true" :unchecked-value="false" />
-									<label for="privacyPolicy">個人情報保護方針に同意する</label>
-								</div>
-								<div class="inline-group">
-									<!-- 利用規約の同意チェック -->
-									<Field name="termsOfUse" type="checkbox" id="termsOfUse" :value="true" :unchecked-value="false" />
-									<label for="termsOfUse">サイト利用規約に同意する</label>
-								</div>
-								<!-- バリデーションエラー表示 -->
-								<ErrorMessage name="privacyPolicy" class="error-message" />
-								<ErrorMessage name="termsOfUse" class="error-message" />
-							</fieldset>
-
-							<!-- 画像ファイルアップロード -->
-							<div class="form-group">
-								<label for="image">画像ファイル</label>
-								<!-- ファイル選択 -->
-								<Field name="image" type="file" id="image" class="form-control" @change="(e) => validateFile(e)" />
-
-								<!-- 選択済みファイル名と削除ボタン -->
-								<div v-if="getUploadedFileName()">
-									<p>選択済みファイル： {{ getUploadedFileName() }}</p>
-									<button @click="removeFile">削除</button>
-								</div>
-
-								<!-- バリデーションエラー表示 -->
-								<ErrorMessage name="image" class="error-message" />
-							</div>
-
-							<!-- 送信ボタン -->
-							<div class="form-group">
-								<button type="submit" class="submit-button">確認画面へ</button>
-							</div>
-						</form>
+	<section class="inquiry">
+		<div class="container">
+			<header class="inquiry--header">
+				<header>
+					<PageTitle title-text="フォーム画面サンプル" />
+					<p>このページはDI-KIT内でBootstrap5を使用し、CSS命名規則を混在させても安全であることを示すサンプルです。</p>
+					<p>
+						不要な場合に削除しやすいようBootstrapのコアファイルはCDNで読み込んでいますが、実際に使用する場合は<a href="https://www.npmjs.com/package/bootstrap/v/5.0.0-alpha1">npm install</a
+						>を推奨しています。
+					</p>
+				</header>
+			</header>
+			<div class="inquiry--body">
+				<!-- フォーム全体 -->
+				<form @submit.prevent="onSubmit">
+					<!-- 名前フィールド -->
+					<div class="form-group">
+						<label for="name">名前<span class="required">*</span></label>
+						<!-- 入力フィールド -->
+						<Field name="name" type="text" id="name" class="form-control" />
+						<!-- バリデーションエラー表示 -->
+						<ErrorMessage name="name" class="error-message" />
 					</div>
-				</div>
-			</section>
-		</main>
-	</div>
+
+					<!-- 年齢フィールド -->
+					<div class="form-group">
+						<label for="age">年齢</label>
+						<!-- 数値入力用フィールド -->
+						<Field name="age" type="text" id="age" class="form-control" />
+						<!-- バリデーションエラー表示 -->
+						<ErrorMessage name="age" class="error-message" />
+					</div>
+
+					<!-- メールアドレスフィールド -->
+					<div class="form-group">
+						<label for="email">メールアドレス<span class="required">*</span></label>
+						<!-- メール入力フィールド -->
+						<Field name="email" type="email" id="email" class="form-control" />
+						<!-- バリデーションエラー表示 -->
+						<ErrorMessage name="email" class="error-message" />
+					</div>
+
+					<!-- 性別フィールド -->
+					<div class="form-group">
+						<label>性別<span class="required">*</span></label>
+						<div class="inline-group">
+							<!-- 男性選択 -->
+							<Field name="gender" type="radio" value="男性" id="male" />
+							<label for="male">男性</label>
+							<!-- 女性選択 -->
+							<Field name="gender" type="radio" value="女性" id="female" />
+							<label for="female">女性</label>
+						</div>
+						<!-- バリデーションエラー表示 -->
+						<ErrorMessage name="gender" class="error-message" />
+					</div>
+
+					<!-- 都道府県選択 -->
+					<div class="form-group">
+						<label for="prefecture">都道府県名<span class="required">*</span></label>
+						<!-- ドロップダウン選択 -->
+						<Field name="prefecture" as="select" id="prefecture" class="form-control">
+							<option value="" disabled>選択してください</option>
+							<!-- 各都道府県の選択肢 -->
+							<option value="北海道">北海道</option>
+							<!-- 以下省略 -->
+						</Field>
+						<!-- バリデーションエラー表示 -->
+						<ErrorMessage name="prefecture" class="error-message" />
+					</div>
+
+					<!-- メッセージフィールド -->
+					<div class="form-group">
+						<label for="message">メッセージ</label>
+						<!-- テキストエリア -->
+						<Field name="message" as="textarea" id="message" class="form-control" />
+						<!-- バリデーションエラー表示 -->
+						<ErrorMessage name="message" class="error-message" />
+					</div>
+
+					<!-- 同意確認フィールド -->
+					<fieldset class="form-group">
+						<legend>同意確認<span class="required">*</span></legend>
+						<div class="inline-group">
+							<!-- プライバシーポリシーの同意チェック -->
+							<Field name="privacyPolicy" type="checkbox" id="privacyPolicy" :value="true" :unchecked-value="false" />
+							<label for="privacyPolicy">個人情報保護方針に同意する</label>
+						</div>
+						<div class="inline-group">
+							<!-- 利用規約の同意チェック -->
+							<Field name="termsOfUse" type="checkbox" id="termsOfUse" :value="true" :unchecked-value="false" />
+							<label for="termsOfUse">サイト利用規約に同意する</label>
+						</div>
+						<!-- バリデーションエラー表示 -->
+						<ErrorMessage name="privacyPolicy" class="error-message" />
+						<ErrorMessage name="termsOfUse" class="error-message" />
+					</fieldset>
+
+					<!-- 画像ファイルアップロード -->
+					<div class="form-group">
+						<label for="image">画像ファイル</label>
+						<!-- ファイル選択 -->
+						<Field name="image" type="file" id="image" class="form-control" @change="(e) => validateFile(e)" />
+
+						<!-- 選択済みファイル名と削除ボタン -->
+						<div v-if="getUploadedFileName()">
+							<p>選択済みファイル： {{ getUploadedFileName() }}</p>
+							<button @click="removeFile">削除</button>
+						</div>
+
+						<!-- バリデーションエラー表示 -->
+						<ErrorMessage name="image" class="error-message" />
+					</div>
+
+					<!-- 送信ボタン -->
+					<div class="form-group">
+						<button type="submit" class="submit-button">確認画面へ</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script setup lang="ts">
